9af963878865f3a8bba02967df8b1981b3a265b7
pdkproitf <phucdaica.pro@gmail.com>
2017-06-22 21:31:13 +0700

    change user tokens style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622111135) do
+ActiveRecord::Schema.define(version: 20170622142025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(version: 20170622111135) do
     t.string   "nickname"
     t.string   "image"
     t.string   "email"
-    t.text     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.jsonb    "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
