54bb99957a618c17d2b0866bdd0c703363b7d995
pdkproitf <phucdaica.pro@gmail.com>
2017-06-23 09:13:10 +0700

    add get category api and get categories

--- app/api/category_api/categories.rb
+++ app/api/category_api/categories.rb
@@ -20,6 +20,19 @@ module CategoryApi
         return_message(I18n.t('success'), CategorySerializer.new(@category))
       end
 
+      desc 'get a category' #, entity: Entities::UserEntities::Users
+      get ':id' do
+        @category = @current_member.bar.categories.find(params[:id])
+        error!(I18n.t('not_found', title: 'Category'), 404) if @category.blank?
+        return_message(I18n.t('success'), CategorySerializer.new(@category))
+      end
+
+      desc 'get all category' #, entity: Entities::UserEntities::Users
+      get '' do
+        @category = @current_member.bar.categories.map { |e| CategorySerializer.new(e) }
+        return_message(I18n.t('success'), @category)
+      end
+
       desc 'update a category' #, entity: Entities::UserEntities::Users
       params do
         requires :category, type: Hash do
@@ -29,7 +42,7 @@ module CategoryApi
       put ':id' do
         @category = @current_member.bar.categories.find(params[:id])
         error!(I18n.t('not_found', title: 'Category'), 404) if @category.blank?
-        @category.update_attributes(name: params[:category][:name])
+        @category.update_attributes!(name: params[:category][:name])
         return_message(I18n.t('success'), CategorySerializer.new(@category))
       end
     end
