66d1e9e9125b235f007dcb98592105c45f841f4d
pdkproitf <phucdaica.pro@gmail.com>
2017-06-23 19:08:07 +0700

    create add/remove beer api for passport

--- app/api/passport_api/passports.rb
+++ app/api/passport_api/passports.rb
@@ -21,6 +21,36 @@ module PassportApi
         passport = Passport.find(params[:id])
         return_message(I18n.t('success'), PassportSerializer.new(passport))
       end
+
+      desc 'add a beer to passport'
+      params do
+        requires :passport_id, type: Integer, desc: 'passport id'
+        requires :beer_id, type: Integer, desc: 'beer_id id'
+      end
+      post 'beer' do
+        passport = Passport.find(params[:passport_id])
+        beer = Beer.find(params[:beer_id])
+
+        passport_beer =  PassportBeer.create!(passport_id: passport.id, beer_id: beer.id)
+        return_message(I18n.t('success'), PassportBeerSerializer.new(passport_beer))
+      end
+
+      desc 'remove a beer to passport'
+      params do
+        requires :passport_id, type: Integer, desc: 'passport id'
+        requires :beer_id, type: Integer, desc: 'beer_id id'
+      end
+      delete 'beer' do
+        passport = Passport.find(params[:passport_id])
+        beer = Beer.find(params[:beer_id])
+
+        passport_beer =  passport.passport_beers.find_by(beer_id: beer.id)
+
+        error!(I18n.t('not_found', title: 'Beer on Passport'), 404) if passport_beer.blank?
+
+        passport_beer.destroy!
+        return_message(I18n.t('success'), PassportBeerSerializer.new(passport_beer))
+      end
     end
   end
 end
