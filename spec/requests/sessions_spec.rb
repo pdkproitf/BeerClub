aaa4ee4762eb2347085a530e9c92b1d0f4a7279f
pdkproitf <phucdaica.pro@gmail.com>
2017-07-03 18:35:23 +0700

    change let(:messages) to let(body) on test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe API::Root::UserApi::Sessions, type: :request do
   describe 'POST api/users/sign-in' do
     let(:url) { '/api/users/sign-in' }
     let!(:user) { FactoryGirl.create(:user) }
-    let(:messages) { JSON.parse(response.body) }
+    let(:body) { JSON.parse(response.body) }
 
     context 'sign-in' do
       it 'wrong credentials' do
@@ -13,7 +13,7 @@ RSpec.describe API::Root::UserApi::Sessions, type: :request do
         post url, params: params
 
         expect(response).to have_http_status(500)
-        expect(messages['error']).to eq(I18n.t('devise_token_auth.sessions.bad_credentials'))
+        expect(body['error']).to eq(I18n.t('devise_token_auth.sessions.bad_credentials'))
       end
 
       it 'right credentials' do
@@ -21,7 +21,7 @@ RSpec.describe API::Root::UserApi::Sessions, type: :request do
         post url, params: params
 
         expect(response).to have_http_status(201)
-        expect(messages['status']).to eq(I18n.t('devise.sessions.signed_in'))
+        expect(body['messages']).to eq(I18n.t('devise.sessions.signed_in'))
       end
     end
   end
