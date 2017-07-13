085332eed8cc744d1512c4bb7266131c133f6d4b
pdkproitf <phucdaica.pro@gmail.com>
2017-07-13 16:49:25 +0700

    adjust factory model  and test job

--- app/controllers/api/entities/beers.rb
+++ app/controllers/api/entities/beers.rb
@@ -1,15 +1,13 @@
 module API
   module Entities
     class Beers < Grape::Entity
-      category = FactoryGirl.create(:category, name: Faker::Name.name)
-      beer = FactoryGirl.create(:beer, category: category)
       expose :id, documentation: { type: 'string', values: [Faker::Number.digit] }
-      expose :name, documentation: { type: 'string', values: [beer.name] }
-      expose :price, documentation: { type: 'string', values: [beer.price] }
-      expose :country, documentation: { type: 'string', values: [beer.country] }
+      expose :name, documentation: { type: 'string', values: [Faker::Name.unique.name] }
+      expose :price, documentation: { type: 'string', values: [Faker::Number.decimal(2)] }
+      expose :country, documentation: { type: 'string', values: [Faker::Address.country] }
       expose :archived, documentation: { type: 'boolean', values: [false] }
-      expose :description, documentation: { type: 'string', values: [beer.description] }
-      expose :manufacurter, documentation: { type: 'string', values: [beer.manufacurter] }
+      expose :description, documentation: { type: 'string', values: [Faker::Lorem.paragraph(2)] }
+      expose :manufacurter, documentation: { type: 'string', values: [Faker::Educator.campus] }
     end
 
     class BeersCategory < Beers
